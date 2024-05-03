d0dc16e9db4d20e501391ad2613f51dc0ff9cef9
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-05-03 15:07:24 -0400

    Display machine and battery info without upower

--- archlive/airootfs/root/bat-health.sh
+++ archlive/airootfs/root/bat-health.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 # Iterate through battery devices and print health data
-echo Battery health script by Joel
+echo Joel\'s Battery Checker v0.2
 echo
-for device in $(upower -e | grep BAT) 
-do
-echo
-upower -i $device | grep -E 'model|energy|capacity';
+echo Family: $(cat /sys/devices/virtual/dmi/id/product_family)
+echo Model: $(cat /sys/devices/virtual/dmi/id/product_name)
+echo Serial: $(cat /sys/devices/virtual/dmi/id/product_serial) # requires root
+echo CPU: $(cat /proc/cpuinfo | grep 'model name' | head -n 1 | cut -c 14-)
+echo Memory: $(free -h | sed -n '2p' | awk '{printf $2}')
 echo 
-done
-echo Press any key to shut down...
+bold=$(tput bold) #bold text
+normal=$(tput sgr0) #normal text
+if ls /sys/class/power_supply | grep --quiet BAT; then
+    for batdir in /sys/class/power_supply/BAT*; do
+        energy_full=$(cat $batdir/energy_full)
+        energy_design=$(cat $batdir/energy_full_design)
+        capacity_percent=$(echo $energy_full $energy_design | awk '{printf "%.1f\n", ($1 / $2) * 100}')%
+        echo Battery model: $(cat $batdir/model_name)
+        echo .  Current charge: $(cat $batdir/capacity)% \($(cat $batdir/status)\)
+        echo .  Full capacity: $energy_full
+        echo .  Design capacity: $energy_design
+        echo .  Battery heath: ${bold}$capacity_percent${normal}
+        echo
+    done
+else
+    echo "No battery info detected"
+    echo
+fi
+
+
+echo Press Enter to shut down...
 read
 poweroff
-
-
