090d75906d3f021a593179ad9919f4b431d61900
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-10-17 10:03:03 -0400

    Replace neofetch with maintained alternative

--- archlive/airootfs/root/bat-health.sh
+++ archlive/airootfs/root/bat-health.sh
@@ -9,7 +9,7 @@ echo Family: $(cat /sys/devices/virtual/dmi/id/product_family)
 echo Model: $(cat /sys/devices/virtual/dmi/id/product_name)
 echo Serial: $(cat /sys/devices/virtual/dmi/id/product_serial) # requires root
 echo CPU: $(cat /proc/cpuinfo | grep 'model name' | head -n 1 | cut -c 14-)
-neofetch gpu | sed "s/gpu/GPU/g" # get GPU info
+fastfetch --logo none | grep GPU | xargs
 echo Memory: $(free -h | sed -n '2p' | awk '{printf $2}')
 echo
 echo Attached Storage:
